99e2eae599fad49fe6c6e2fc736bdec8ab51aa3b
mgurski <mateuszgurski444@gmail.com>
2020-11-15 06:09:49 +0100

    GPU Version

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,4 +1,4 @@
-
+import hurry.filesize
 
 def load_text_from_file(path: str):
     """Loads a textfile from the given path and returns it"""
@@ -11,3 +11,11 @@ def generate_text(textsize):
 def save_text_to_file(text, path):
     """Saves given text""" 
     pass
+
+def utf8len(s):
+    return len(s.encode('utf-8'))
+
+def get_file_size(text):
+    """Returns file size in MB"""
+    return hurry.filesize.size(utf8len(text))
+
