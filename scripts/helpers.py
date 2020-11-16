efdcfcfc432107096bce42141298350ec72c691c
mgurski <mateuszgurski444@gmail.com>
2020-11-16 03:20:48 +0100

    Generating text of given size in MB

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,11 +1,19 @@
 import hurry.filesize
 import string
 import random
+import lorem
+import math
 
 def generate_text(length: int):
     letters = string.ascii_letters + string.whitespace.replace('\x0b\x0c','')
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
+
+def generate_text_in_mb(size: int):
+    text = generate_text(1000)
+    size = size * 1000000
+    text = text*math.ceil((size/int(1000)))
+    return text
 
 def load_text_from_file(path: str):
     """Loads a textfile from the given path and returns it"""
